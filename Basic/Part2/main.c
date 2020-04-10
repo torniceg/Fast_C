cf5705aeaa1cb508fffc67a8648b66c2fa9cadf0
torniceg <61825523+torniceg@users.noreply.github.com>
2020-04-10 19:31:00 +0900

    first c

--- Basic/Part2/main.c
+++ Basic/Part2/main.c
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <limits.h>
 
 int main(void) {
 	/*
@@ -38,6 +39,133 @@ int main(void) {
 	int a = 7, b = 7;
 	printf("%d\n", (a == b) ? 100 : -100);
 	*/
+	/*
+	int a[10] = { 0, };
+	int i;
+	for (i = 0; i < 10; i++) {
+		printf("%d ", a[i]);
+	}
+	*/
+	/*
+	int a[10] = { 6,5,4,3,9,8,0,1,2,7 };
+	int i, maxValue = INT_MIN;
+	for (i = 0; i < 10; i++) {
+		if (maxValue < a[i]) {
+			maxValue = a[i];
+		}
+	}
+	printf("%d\n", maxValue);
+	*/
+	/*
+	char a[20];
+	scanf("%s", &a);
+	printf("%s\n", a);
+	*/
+	/*
+	char a[20] = "Hello World";
+	a[4] = ',';
+	printf("%s", a);
+	*/
+	/*
+	char a[] = "Hello World";
+	int count = 0;
+	for (int i = 0; i <= 10; i++) {
+		if (a[i] == 'l') {
+			count++;
+		}
+	}
+	printf("%d\n", count);
+	*/
+	/*
+	int a = 5;
+	int* b = &a;
+	printf("%d\n", *b);
+	*/
+	/*
+	int a[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int i;
+	for ( i = 0; i < 10; i++)
+	{
+		printf("%d\n", &a[i]);
+	}
+	*/
+	/*
+	int a = 5;
+	int* b = &a;
+	int** c = &b;
+	printf("%d\n", a);
+	printf("%d\n", &a);
+	printf("%d\n", b);
+	printf("%d\n", &b);
+	printf("%d\n", *b);
+	printf("%d\n", c);
+	printf("%d\n", &c);
+	printf("%d\n", *c);
+	printf("%d\n", **c);
+	*/
+	/*
+	int a[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int* b = a;
+	printf("%d\n", a);
+	printf("%d\n", &a);
+	printf("%d\n", *a);
+	printf("%d\n", b);
+	printf("%d\n", &b);
+	printf("%d\n", *b);
+	printf("%d\n", b[0]);
+	printf("%d\n", b[1]);
+	*/
+	
+	/*
+	int a;
+	char c;
+	scanf("%d", &a);
+	printf("%d\n", a);
+	int temp;
+	while ((temp = getchar()) != EOF && temp != '\n') {}
+	scanf("%c", &c);
+	printf("%c\n", c);
+	*/
+	/*
+	char* a = "Hello World";
+	printf("%s\n", a);
+	printf("%c\n", a[1]);
+	printf("%c\n", a[4]);
+	printf("%c\n", a[8]);
+	*/
+	/*
+	char a[100];
+	gets(a);
+	gets_s(a, sizeof(a));
+	printf("%s\n", a);
+	char a[100];
+	printf("%s\n", a);
+	*/
+	/*
+	char a[20] = "Sung Woo";
+	printf("문자열의 길이: %d\n", strlen(a));
+	*/
+	/*
+	char a[20] = "Sung Woo";
+	char b[20] = "Hojoon Seok";
+	printf("두 배열의 사전 순 비교: %d\n", strcmp(a,b));
+	*/
+	/*
+	char a[20] = "Sung Woo";
+	char b[20] = "Hojoon Seok";
+	strcpy(a, b);
+	printf("복사된 문자열: %s\n", a);
+	*/
+	/*
+	char a[30] = "My name is ";
+	char b[20] = "Sung Woo";
+	strcat(a, b);
+	printf("합쳐진 결과 문자열: %s\n", a);
+	*/
+
+	char a[20] = "I like you";
+	char b[20] = "like";
+	printf("찾은 문자열: %s\n", strstr(a, b));
 
 	system("pause");
 	return 0;
---
+++ Basic/Part3/main.c
@@ -0,0 +1,88 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <limits.h>
+/*
+int a = 5;
+
+void changeValue() {
+	a = 10;
+}
+
+void process() {
+	static int a = 5;
+	a = a + 1;
+	printf("%d\n", a);
+}
+
+void add(int* a, int b) {
+	*a = *a + b;
+}
+*/
+
+//int a[3][3] = { {1,2,3}, {4,5,6}, {7,8,9} };
+//int a[2][3][3] = { { {1,2,3}, {4,5,6}, {7,8,9} }, { {1,2,3}, {4,5,6}, {7,8,9} } };
+
+int main(void) {
+	/*
+	printf("%d\n", a);
+	changeValue();
+	printf("%d\n", a);
+	*/
+	/*
+	int a = 7;
+	if (1) {
+		int a = 5;
+	}
+	printf("%d\n", a);
+	*/
+	/*
+	process();
+	*/
+	/*
+	register int a = 10, i;
+	for ( i = 0; i < a; i++)
+	{
+		printf("%d ", i);
+	}
+	*/
+	/*
+	int a = 7;
+	add(&a, 10);
+	printf("%d\n", a);
+	*/
+	/*
+	int i, j;
+	for ( i = 0; i < 3; i++)
+	{
+		for ( j = 0; j < 3; j++)
+		{
+			printf("%d ", a[i][j]);
+		}
+		printf("\n");
+	}
+	*/
+	/*
+	int i, j, k;
+	for (i = 0; i < 2; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			for ( k = 0; k < 3; k++)
+			{
+				printf("%d ", a[i][j][k]);
+			}
+			printf("\n");
+		}
+		printf("\n");
+	}
+	*/
+
+	int a = 10;
+	int b[10];
+
+
+	system("pause");
+	return 0;
+}
+
+
