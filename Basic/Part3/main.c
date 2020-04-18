d032448aa10c8c1c7d0565e1dba42478d40b5795
torniceg <61825523+torniceg@users.noreply.github.com>
2020-04-18 23:32:53 +0900

    add part4

--- Basic/Part3/main.c
+++ Basic/Part3/main.c
@@ -76,10 +76,42 @@ int main(void) {
 		printf("\n");
 	}
 	*/
-
+	/*
 	int a = 10;
 	int b[10];
-
+	b = &a;
+	*/
+	/*
+	int a[5] = { 1,2,3,4,5 };
+	int* b = a;
+	printf("%d\n", b[2]);
+	*/
+	/*
+	int a[5] = { 1,2,3,4,5 };
+	int i;
+	for ( i = 0; i < 5; i++)
+	{
+		printf("%d ", *(a + i));
+	}
+	*/
+	/*
+	double b[10];
+	printf("%d %d\n", b, b + 9);
+	*/
+	/*
+	int a[5] = { 1,2,3,4,5 };
+	int* p = a;
+	printf("%d\n", *(p++));
+	printf("%d\n", *(++p));
+	printf("%d\n", *(p+2));
+	*/
+	int a[2][5] = { {1,2,3,4,5},{6,7,8,9,10} };
+	int (*p)[5] = a[1];
+	int i;
+	for ( i = 0; i < 5; i++)
+	{
+		printf("%d ", p[0][i]);
+	}
 
 	system("pause");
 	return 0;
---
+++ Basic/Part4/main.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+	/*
+	printf("%d\n", sizeof(int));
+	int* a = malloc(sizeof(int));
+	printf("%d\n", a);
+	free(a);
+	a = malloc(sizeof(int));
+	printf("%d\n", a);
+	free(a);
+	*/
+	/*
+	char* a = malloc(100);
+	memset(a, 'A', 100);
+	for (int i = 0; i < 100; i++)
+	{
+		printf("%c ", a[i]);
+	}
+	*/
+
+	int** p = (int**)malloc(sizeof(int*) * 3);
+
+
+	system("pause");
+	return 0;
+}
