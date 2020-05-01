a2f584a7ea0f5cc13076d4594955e217b08eb0d1
torniceg <61825523+torniceg@users.noreply.github.com>
2020-05-01 19:17:01 +0900

    Part4 end

--- Basic/Part4/main.c
+++ Basic/Part4/main.c
@@ -23,7 +23,26 @@ int main(void) {
 	*/
 
 	int** p = (int**)malloc(sizeof(int*) * 3);
-	//test
+	for (int i = 0; i < 3; i++)
+	{
+		*(p + i) = (int*)malloc(sizeof(int) * 3);
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			*(*(p + i) + j) = i * 3 + j;
+		}
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			printf("%d ", *(*(p + i) + j));
+		}
+		printf("\n");
+	}
+
 
 	system("pause");
 	return 0;
