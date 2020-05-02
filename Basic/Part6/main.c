f690fa2ccb7fc1d5f283dabdb5cecd53c2034678
torniceg <61825523+torniceg@users.noreply.github.com>
2020-05-02 15:35:09 +0900

    basic end

---
+++ Basic/Part6/main.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+
+	//±¸Á¶Ã¼
+
+	system("pause");
+	return 0;
+}
---
+++ Basic/Part8/main.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "temp.h"
+#define PI 3.1415926535
+#define POW(x) (x*x)
+#define ll long long
+#define ld long double
+
+int main(void) {
+
+	printf("%d\n", add(3, 7));
+
+	//int r = 10;
+	//printf("원의 둘레: %.2f\n", 2 * PI * r);
+	//printf("r의 제곱: %d\n", POW(r));
+
+	//ll a = 9865431000;
+	//ld b = 100.5054;
+	//printf("%.1f\n", a * b);
+
+
+
+	system("pause");
+	return 0;
+}
